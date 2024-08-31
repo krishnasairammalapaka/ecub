544bcb390471f355f2681e5676a377ab611f876d
Karuppasamy K <karuppan.the.pentester@gmail.com>
2024-08-31 10:21:57 +0530

    Rated

--- lib/service_page/FoodService/t2.dart
+++ lib/service_page/FoodService/t2.dart
@@ -14,8 +14,8 @@ class _FS_RestaurantScreenState extends State<FS_RestaurantScreen> {
   String hotelMail = '';
   String hotelAddress = '';
   String hotelUsername = '';
-  double averageRating = 0.0;
   List<Map<String, dynamic>> foodItems = [];
+  bool isLoading = true;
 
   @override
   void didChangeDependencies() {
@@ -23,28 +23,35 @@ class _FS_RestaurantScreenState extends State<FS_RestaurantScreen> {
     _fetchHotelInfo();
   }
 
+
   void _fetchHotelInfo() async {
     final args = ModalRoute.of(context)?.settings.arguments as Map;
-    final hotelUsername = args['username'];
+    final hotelUsernameArg = args['username'];
+
 
     final hotelQuerySnapshot = await _firestore
         .collection('fs_hotels')
-        .where('hotelUsername', isEqualTo: hotelUsername)
+        .where('hotelUsername', isEqualTo: hotelUsernameArg)
         .get();
 
     if (hotelQuerySnapshot.docs.isNotEmpty) {
       final hotelData = hotelQuerySnapshot.docs.first.data();
       setState(() {
-        hotelName = hotelData['hotelName'];
-        hotelMail = hotelData['hotelMail'];
-        hotelAddress = hotelData['hotelAddress'];
-        this.hotelUsername = hotelData['hotelUsername'];
+        hotelName = hotelData['hotelName'] ?? '';
+        hotelMail = hotelData['hotelMail'] ?? '';
+        hotelAddress = hotelData['hotelAddress'] ?? '';
+        hotelUsername = hotelData['hotelUsername'] ?? '';
       });
       _fetchFoodItems();
     } else {
+
       print('No hotel found with the given username.');
+      setState(() {
+        isLoading = false;
+      });
     }
   }
+
 
   void _fetchFoodItems() async {
     final foodItemsSnapshot = await _firestore
@@ -53,43 +60,71 @@ class _FS_RestaurantScreenState extends State<FS_RestaurantScreen> {
         .get();
 
     if (foodItemsSnapshot.docs.isNotEmpty) {
-      List<Map<String, dynamic>> tempFoodItems = [];
+      List<Map<String, dynamic>> items = [];
 
       for (var doc in foodItemsSnapshot.docs) {
-        double averageRating = await _calculateAverageRating(doc['productId']);
-        tempFoodItems.add({
-          'name': doc['productTitle'],
-          'restaurant': doc['productOwnership'],
-          'price': doc['productPrice'],
-          'image': doc['productImg'],
-          'id': doc['productId'],
-          'desc': doc['productDesc'],
-          'averageRating': averageRating,
+        String productId = doc['productId'];
+        double averageRating = await _fetchAverageRating(productId);
+
+        items.add({
+          'name': doc['productTitle'] ?? '',
+          'restaurant': doc['productOwnership'] ?? '',
+          'price': (doc['productPrice'] ?? 0),
+          'image': doc['productImg'] ?? '',
+          'id': doc['productId'] ?? '',
+          'desc': doc['productDesc'] ?? '',
+          'rating': averageRating,
         });
       }
 
+
+      double maxRating = items.isNotEmpty
+          ? items.map((item) => item['rating'] as double).reduce((a, b) => a > b ? a : b)
+          : 0.0;
+
       setState(() {
-        foodItems = tempFoodItems;
+        foodItems = items.map((item) {
+          item['isTopRated'] = item['rating'] == maxRating;
+          return item;
+        }).toList();
+        isLoading = false;
       });
+
+      print('Fetched food items: $foodItems');
+    } else {
+      setState(() {
+        foodItems = [];
+        isLoading = false;
+      });
+      print('No food items found for hotelUsername: $hotelUsername');
     }
   }
 
-  Future<double> _calculateAverageRating(String productId) async {
-    final ratingsSnapshot = await _firestore
+
+  Future<double> _fetchAverageRating(String productId) async {
+    final commentsSnapshot = await _firestore
         .collection('fs_comments')
         .where('productId', isEqualTo: productId)
         .get();
 
-    if (ratingsSnapshot.docs.isEmpty) {
+    if (commentsSnapshot.docs.isEmpty) {
       return 0.0;
     }
 
-    double totalRating = 0.0;
-    for (var doc in ratingsSnapshot.docs) {
-      totalRating += doc['rating'];
-    }
+    double totalRating = commentsSnapshot.docs
+        .map((doc) {
+      var rating = doc['rating'];
+      if (rating is int) {
+        return rating.toDouble();
+      } else if (rating is double) {
+        return rating;
+      } else {
+        return 0.0;
+      }
+    })
+        .fold(0.0, (prev, element) => prev + element);
 
-    return totalRating / ratingsSnapshot.docs.length;
+    return totalRating / commentsSnapshot.docs.length;
   }
 
   @override
@@ -117,17 +152,19 @@ class _FS_RestaurantScreenState extends State<FS_RestaurantScreen> {
             icon: Icon(Icons.favorite_border, color: Color(0xFF0D5EF9)),
             onPressed: () {
 
-              // Add your favorite button action here
             },
           ),
         ],
       ),
-      body: SingleChildScrollView(
+      body: isLoading
+          ? Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+
               Container(
                 width: double.infinity,
                 height: 200,
@@ -141,25 +178,31 @@ class _FS_RestaurantScreenState extends State<FS_RestaurantScreen> {
                 ),
               ),
               SizedBox(height: 16),
+
               FutureBuilder(
                 future: Translate.translateText(hotelName),
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
-                    return Text(snapshot.data!,
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ));
+                    return Text(
+                      snapshot.data!,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
                   } else {
-                    return Text(hotelName,
-                        style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                        ));
+                    return Text(
+                      hotelName,
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
                   }
                 },
               ),
               SizedBox(height: 8),
+
               Text(
                 hotelMail,
                 style: TextStyle(
@@ -167,6 +210,7 @@ class _FS_RestaurantScreenState extends State<FS_RestaurantScreen> {
                 ),
               ),
               SizedBox(height: 8),
+
               FutureBuilder(
                 future: Translate.translateText(hotelAddress),
                 builder: (context, snapshot) {
@@ -188,6 +232,7 @@ class _FS_RestaurantScreenState extends State<FS_RestaurantScreen> {
                 },
               ),
               SizedBox(height: 16),
+
               Row(
                 children: [
                   Icon(Icons.star, color: Color(0xFF0D5EF9)),
@@ -218,23 +263,32 @@ class _FS_RestaurantScreenState extends State<FS_RestaurantScreen> {
                 ],
               ),
               SizedBox(height: 16),
-              GridView.count(
+
+              foodItems.isEmpty
+                  ? Text('No food items available.')
+                  : GridView.count(
                 crossAxisCount: 2,
                 crossAxisSpacing: 16,
                 mainAxisSpacing: 16,
                 shrinkWrap: true,
                 physics: NeverScrollableScrollPhysics(),
-                children: foodItems
-                    .map((item) => MenuItem(
-                  name: item['name'],
-                  restaurant: item['restaurant'],
-                  price: item['price'].toInt(),
-                  image: item['image'],
-                  id: item['id'],
-                  desc: item['desc'],
-                  averageRating: averageRating,
-                ))
-                    .toList(),
+                children: foodItems.map((item) {
+                  double rating = (item['rating'] is num)
+                      ? (item['rating'] as num).toDouble()
+                      : 0.0;
+                  bool isTopRated = item['isTopRated'] ?? false;
+
+                  return MenuItem(
+                    name: item['name'] ?? '',
+                    restaurant: item['restaurant'] ?? '',
+                    price: (item['price'] ?? 0).toInt(),
+                    image: item['image'] ?? '',
+                    id: item['id'] ?? '',
+                    desc: item['desc'] ?? '',
+                    rating: rating,
+                    isTopRated: isTopRated,
+                  );
+                }).toList(),
               ),
             ],
           ),
@@ -276,7 +330,8 @@ class MenuItem extends StatelessWidget {
   final String image;
   final String desc;
   final String restaurant;
-  final double averageRating;
+  final double rating;
+  final bool isTopRated;
 
   MenuItem({
     required this.name,
@@ -285,55 +340,34 @@ class MenuItem extends StatelessWidget {
     required this.image,
     required this.id,
     required this.desc,
-    required this.averageRating,
+    required this.rating,
+    required this.isTopRated,
   });
-
-  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
-
-  Future<double> _calculateAverageRating(String productId) async {
-    final ratingsSnapshot = await _firestore
-        .collection('fs_comments')
-        .where('foodId', isEqualTo: productId)
-        .get();
-
-    if (ratingsSnapshot.docs.isEmpty) {
-      return 0.0;
-    }
-
-    double totalRating = 0.0;
-    for (var doc in ratingsSnapshot.docs) {
-      totalRating += doc['rating'];
-    }
-
-    return totalRating / ratingsSnapshot.docs.length;
-  }
-
-
 
   @override
   Widget build(BuildContext context) {
     final assetImage = image;
-
-
-    return FutureBuilder<double>(
-      future: _calculateAverageRating(id),
+    return FutureBuilder<List<String?>>(
+      future: Future.wait([
+        Translate.translateText(name),
+        Translate.translateText(desc),
+        Translate.translateText(restaurant),
+      ]),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return Center(child: CircularProgressIndicator());
         } else if (snapshot.hasError) {
-          return Center(child: Text('Error loading rating'));
+          return Center(child: Text('Error loading translations'));
         } else {
-          final averageRating = snapshot.data ?? 0.0;
-          print(averageRating);
           return GestureDetector(
             onTap: () {
               Navigator.pushNamed(context, '/fs_product', arguments: {
                 'id': id,
-                'title': name,
+                'title': snapshot.data![0] ?? name,
                 'price': price,
                 'image': assetImage,
-                'description': desc,
-                'shop': restaurant,
+                'description': snapshot.data![1] ?? desc,
+                'shop': snapshot.data![2] ?? restaurant,
               });
             },
             child: Container(
@@ -353,6 +387,7 @@ class MenuItem extends StatelessWidget {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+
                   Container(
                     height: 80,
                     decoration: BoxDecoration(
@@ -365,15 +400,32 @@ class MenuItem extends StatelessWidget {
                     ),
                   ),
                   SizedBox(height: 12),
-                  Text(
-                    name,
-                    style: TextStyle(
-                      fontSize: 16,
-                      overflow: TextOverflow.ellipsis,
-                      fontWeight: FontWeight.bold,
-                    ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(
+                        child: Text(
+                          snapshot.data![0] ?? name,
+                          style: TextStyle(
+                            fontSize: 16,
+                            overflow: TextOverflow.ellipsis,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 4),
+                      Text(
+                        rating.toStringAsFixed(1),
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ],
                   ),
                   SizedBox(height: 10),
+
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -387,7 +439,7 @@ class MenuItem extends StatelessWidget {
                         ),
                       ),
                       SizedBox(width: 1),
-                      if (averageRating > 4.5)
+                      if (isTopRated)
                         Image.asset(
                           'assets/toprated.png',
                           height: 35,
